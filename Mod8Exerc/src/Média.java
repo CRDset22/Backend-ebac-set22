102efed1a2472309256fb4327a1ce71f5f769884
Cleber RD <roberto.doria7@gmail.com>
2022-11-08 02:03:41 -0300

    Execicio Modulo 8

---
+++ Mod8Exerc/src/Média.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Média {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int num3 = sc.nextInt();
+		int num4 = sc.nextInt();
+		
+		int soma = (num1 + num2 + num3 + num4) / 4;
+		
+		System.out.println("Média = " + soma);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
